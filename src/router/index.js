31ab2489d2d6db9d7537f640a327993765f3bade
Sunshine-Queen <1053710934@qq.com>
2021-11-16 09:35:52 +0800

    JD

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,69 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: () =>
+            import ( /* webpackChunkName: "home" */ '../views/home/Home.vue')
+    }, {
+        path: '/shop',
+        name: 'Shop',
+        component: () =>
+            import ( /* webpackChunkName: "shop" */ '../views/shop/Shop.vue')
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () =>
+            import ( /* webpackChunkName: "register" */ '../views/register/Register.vue'),
+        beforeEnter(to, from, next) {
+            const { isLogin } = localStorage;
+            isLogin ? next({ name: 'Home' }) : next();
+        }
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () =>
+            import ( /* webpackChunkName: "login" */ '../views/login/Login.vue'),
+        beforeEnter(to, from, next) {
+            const { isLogin } = localStorage;
+            // if (isLogin) {
+            //     next({ name: 'Home' })
+            // }
+            // next();
+            isLogin ? next({ name: 'Home' }) : next();
+        }
+    },
+    //   {
+    //       path: '/about',
+    //       name: 'About',
+    //       // route level code-splitting
+    //       // this generates a separate chunk (about.[hash].js) for this route
+    //       // which is lazy-loaded when the route is visited.
+    //       component: () =>
+    //           import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    //   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
+
+router.beforeEach((to, from, next) => {
+    //     const isLogin = localStorage.isLogin;
+    //     if (isLogin || to.name === "Login") {
+    //         next();
+    //     } else {
+    //         next({ name: 'Login' })
+    //     }
+    //     //console.log(to, from);
+    //     next();
+    const { isLogin } = localStorage;
+    const { name } = to;
+    const isLoginOrRegister = (name === "Login" || name === "Register");
+    (isLogin || isLoginOrRegister) ? next(): next({ name: 'Login' });
+})
 export default router
---
+++ src/views/shop/Shop.vue
@@ -0,0 +1,19 @@
+<template>
+    <ShopInfo />
+</template>
+
+<script>
+import ShopInfo from '../../components/ShopInfo.vue'
+export default{
+    name:'Shop',
+    components:{ShopInfo},
+    setup(){
+        
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
