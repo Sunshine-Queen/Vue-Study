d6cdc3200618f871c97581db2603f5a876966e2c
Sunshine-Queen <1053710934@qq.com>
2021-12-02 16:57:11 +0800

    1202

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -67,13 +67,13 @@
 import {computed,ref} from 'vue'
 import {useStore} from 'vuex'
 import {useRoute} from 'vue-router'
-import{useCommonCartEffect} from './commonCartEffect'
+import{useCommonCartEffect} from '../../effects/cartEffects'
 
 
 //获取购物车信息逻辑
 const useCartEffect=(shopId)=>{
     const store=useStore()
-    const {cartList,changeCartItemInfo}=useCommonCartEffect()
+    const {cartList,productList,changeCartItemInfo}=useCommonCartEffect(shopId)
     
 
     const calculations=computed(()=>{
@@ -97,10 +97,7 @@ const useCartEffect=(shopId)=>{
 
         
 
-        const productList=computed(()=>{
-            const productList=cartList[shopId]?.productList||[]
-            return productList
-        })
+        
 
         const changeCartItemCheck=(shopId,productId)=>{
             store.commit('changeCartItemCheck',{
